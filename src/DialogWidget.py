0b9a88bb2988fccd36ce06abbf85dd415d8b660a
Patryk Chrabaszcz <chrabaszcz.patryk@gmail.com>
2017-11-27 23:17:10 +0100

    Add nice feature, remove duplicates from categories list

--- src/DialogWidget.py
+++ src/DialogWidget.py
@@ -1,6 +1,8 @@
 from PyQt5.QtWidgets import QTextEdit
 from PyQt5.Qt import QColor
-
+import pyttsx3
+import os
+from subprocess import Popen
 
 class DialogWidget(QTextEdit):
     def __init__(self, parent=None):
@@ -17,7 +19,11 @@ class DialogWidget(QTextEdit):
 
     def bot_text(self, message):
         self.setTextColor(QColor(0, 0, 255))
+        # engine = pyttsx3.init()
+        # engine.say(message)
+        Popen("espeak \"%s\"" % message, shell=True)
         self.append("Bot: \t%s" % message)
+
 
     def user_message_entered(self, message):
         self.user_text(message)
